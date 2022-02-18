40532e8b1d99c19415054ced1ef154eb2c4a2164
Chris Cowan <agentme49@gmail.com>
2022-02-18 01:07:34 -0800

    run prettier on everything

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-rcon-ts
-==============
+# rcon-ts
+
 A modern RCON client implementation written in TypeScript (targeting ES2015) and is async/await friendly.
 
 (Originally `node-modern-rcon`.)
@@ -19,17 +19,18 @@ Creates a new `Rcon` object.
 ```typescript
 import Rcon from 'rcon-ts';
 const rcon = new Rcon({
-  host: "host-path",
-  port: 25575 /*default*/, 
-  password: "required",
-  timeout: 5000 /*default (5 seconds)*/
+  host: 'host-path',
+  port: 25575 /*default*/,
+  password: 'required',
+  timeout: 5000 /*default (5 seconds)*/,
 });
-````
+```
 
 ### Connecting
 
 Connects with the credentials provided in the constructor.
 Can be awaited on.
+
 ```typescript
 rcon.connect();
 ```
@@ -39,63 +40,60 @@ rcon.connect();
 Executes the provided command on the active connection and returns the response.
 
 ```typescript
-let response = await rcon.send("[rcon request]");
-````
+let response = await rcon.send('[rcon request]');
+```
+
 ### Disconnecting
 
 Ends the current socket and subsequently signals to any pending request that the connection was disconnected.
 
 ```typescript
 rcon.disconnect();
-````
+```
 
 ### Sessions
 
-To simplify the usage, you can use the ```session``` method.
+To simplify the usage, you can use the `session` method.
 
 ```typescript
 // Single command:
 rcon
-  .session(c => c.send("one")) // connects, sends, and then disconnects.
-  .then(()=>console.log("first session complete"), console.error);
-  
+  .session((c) => c.send('one')) // connects, sends, and then disconnects.
+  .then(() => console.log('first session complete'), console.error);
+
 // Multiple commands:
 rcon
-  .session(async c => {
-    await c.send("two");
-    await c.send("three");
+  .session(async (c) => {
+    await c.send('two');
+    await c.send('three');
   })
-  .then(()=>console.log("second session complete"), console.error);
-````
+  .then(() => console.log('second session complete'), console.error);
+```
 
 Any number of conccurrent sessions can be active on a single connection and once they've all completed the connection will end.
 
-```session``` takes a promise factory as a parameter and passes the current Rcon instance as the first param (optional) to the factory.
+`session` takes a promise factory as a parameter and passes the current Rcon instance as the first param (optional) to the factory.
 
 ## Usage Examples
 
 ```typescript
-async function sendHelp()
-{
+async function sendHelp() {
   rcon.connect();
   // safe to immediately setup requests without waiting.
   console.log(await rcon.send('/help'));
   rcon.disconnect();
 }
 
-sendHelp().finally(() =>
-{
+sendHelp().finally(() => {
   const errors = rcon.errors;
-  if(errors.length) console.warn("Errors:",errors);
+  if (errors.length) console.warn('Errors:', errors);
 });
 ```
 
 Or simply...
 
 ```typescript
-rcon
-  .session(c => c.send('/help'))
-  .then(console.log, console.error);
+rcon.session((c) => c.send('/help')).then(console.log, console.error);
 ```
 
 ### Factorio
--- src/index.ts
+++ src/index.ts
@@ -1,265 +1,266 @@
 import * as net from 'net';
-import {Buffer} from 'buffer';
+import { Buffer } from 'buffer';
 
 export class ExtendableError extends Error {
-	constructor(message: string = '', public readonly innerException?: Error) {
-		super(message);
-		this.message = message;
-		this.name = this.constructor.name;
-		Error.captureStackTrace(this, this.constructor);
-	}
+  constructor(message: string = '', public readonly innerException?: Error) {
+    super(message);
+    this.message = message;
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
 }
 
 export class RconError extends ExtendableError {
-	constructor(message: string, innerException?: Error) {
-		super(message, innerException);
-		Object.freeze(this);
-	}
+  constructor(message: string, innerException?: Error) {
+    super(message, innerException);
+    Object.freeze(this);
+  }
 }
 
 export enum State {
-	Disconnected = 0,
-	Connecting = 0.5,
-	Connected = 1,
-	Authorized = 2,
-	Refused = -1,
-	Unauthorized = -2
+  Disconnected = 0,
+  Connecting = 0.5,
+  Connected = 1,
+  Authorized = 2,
+  Refused = -1,
+  Unauthorized = -2,
 }
 
 enum PacketType {
-	AUTH = 0x03, // outgoing
-	COMMAND = 0x02, // outgoing
-	RESPONSE_AUTH = 0x02 // incoming
+  AUTH = 0x03, // outgoing
+  COMMAND = 0x02, // outgoing
+  RESPONSE_AUTH = 0x02, // incoming
 }
 
 type Callback = (data: string | null, error?: Error) => void;
 
 export interface RconConfig {
-	host: string;
-	port?: number;
-	password: string;
-	timeout?: number;
+  host: string;
+  port?: number;
+  password: string;
+  timeout?: number;
 }
 
 const Defaults = {
-	PORT: 25575,
-	TIMEOUT: 5000,
+  PORT: 25575,
+  TIMEOUT: 5000,
 };
 
 export class Rcon implements RconConfig {
+  readonly host: string;
+  readonly port: number;
+  readonly password: string;
+  readonly timeout: number;
 
-	readonly host: string;
-	readonly port: number;
-	readonly password: string;
-	readonly timeout: number;
+  enableConsoleLogging: boolean = false;
 
-	enableConsoleLogging: boolean = false;
+  private _authPacketId: number = NaN;
+  private _state: State = State.Disconnected;
+  private _socket: net.Socket | undefined;
+  private _lastRequestId: number = 0xf4240;
+  private _callbacks: Map<number, Callback> = new Map();
+  private _errors: Error[] = [];
+  private _connector: Promise<Rcon> | undefined;
+  private _sessionCount: number = 0;
 
-	private _authPacketId: number = NaN;
-	private _state: State = State.Disconnected;
-	private _socket: net.Socket | undefined;
-	private _lastRequestId: number = 0xF4240;
-	private _callbacks: Map<number, Callback> = new Map();
-	private _errors: Error[] = [];
-	private _connector: Promise<Rcon> | undefined;
-	private _sessionCount:number = 0;
+  get errors(): Error[] {
+    return this._errors.slice();
+  }
 
-	get errors(): Error[] {
-		return this._errors.slice();
-	}
+  get state(): State {
+    return this._state;
+  }
 
-	get state(): State {
-		return this._state;
-	}
+  constructor(config: RconConfig) {
+    let host = config.host;
+    this.host = host = host && host.trim();
+    if (!host) throw new TypeError('"host" argument cannot be empty');
 
-	constructor(config: RconConfig) {
-		let host = config.host;
-		this.host = host = host && host.trim();
-		if (!host)
-			throw new TypeError('"host" argument cannot be empty');
+    this.port = config.port || Defaults.PORT;
 
-		this.port = config.port || Defaults.PORT;
+    const password = config.password;
+    if (!password || !password.trim())
+      throw new TypeError('"password" argument cannot be empty');
 
-		const password = config.password;
-		if (!password || !password.trim())
-			throw new TypeError('"password" argument cannot be empty');
+    this.password = password;
+    this.timeout = config.timeout || Defaults.TIMEOUT;
+  }
 
-		this.password = password;
-		this.timeout = config.timeout || Defaults.TIMEOUT;
-	}
+  connect(): Promise<Rcon> {
+    const _ = this;
+    let p = _._connector;
+    if (!p)
+      _._connector = p = new Promise<Rcon>((resolve, reject) => {
+        _._state = State.Connecting;
+        if (_.enableConsoleLogging)
+          console.log(this.toString(), 'Connecting...');
+        const s = (_._socket = net.createConnection(_.port, _.host));
 
-	connect(): Promise<Rcon> {
-		const _ = this;
-		let p = _._connector;
-		if (!p) _._connector = p = new Promise<Rcon>((resolve, reject) => {
-			_._state = State.Connecting;
-			if (_.enableConsoleLogging) console.log(this.toString(), "Connecting...");
-			const s = _._socket = net.createConnection(_.port, _.host);
+        function cleanup(message?: string, error?: Error): RconError | void {
+          if (error) _._errors.push(error);
+          s.removeAllListeners();
+          if (_._socket == s) _._socket = undefined;
+          if (_._connector == p) _._connector = undefined;
+          if (message) {
+            if (_.enableConsoleLogging) console.error(_.toString(), message);
+            if (message) return new RconError(message, error);
+          }
+        }
 
-			function cleanup(message?: string, error?: Error): RconError | void {
-				if (error) _._errors.push(error);
-				s.removeAllListeners();
-				if (_._socket == s) _._socket = undefined;
-				if (_._connector == p) _._connector = undefined;
-				if (message) {
-					if (_.enableConsoleLogging) console.error(_.toString(), message);
-					if (message) return new RconError(message, error);
-				}
+        // Look for connection failure...
+        s.once('error', (error) => {
+          _._state = State.Refused;
+          reject(cleanup('Connection refused.', error)); // ** First point of failure.
+        });
 
-			}
+        // Look for successful connection...
+        s.once('connect', () => {
+          s.removeAllListeners('error');
+          _._state = State.Connected;
+          if (_.enableConsoleLogging)
+            console.log(_.toString(), 'Connected. Authorizing ...');
 
-			// Look for connection failure...
-			s.once('error', error => {
-				_._state = State.Refused;
-				reject(cleanup("Connection refused.", error)); // ** First point of failure.
-			});
+          s.on('data', (data) => _._handleResponse(data));
 
-			// Look for successful connection...
-			s.once('connect', () => {
-				s.removeAllListeners('error');
-				_._state = State.Connected;
-				if (_.enableConsoleLogging) console.log(_.toString(), "Connected. Authorizing ...");
+          s.on('error', (error) => {
+            _._errors.push(error);
+            if (_.enableConsoleLogging) console.error(_.toString(), error);
+          });
 
-				s.on('data', data => _._handleResponse(data));
+          _._send(_.password, PacketType.AUTH)
+            .then(() => {
+              _._state = State.Authorized;
+              if (_.enableConsoleLogging)
+                console.log(_.toString(), 'Authorized.');
+              resolve(_);
+            })
+            .catch((error) => {
+              _._state = State.Unauthorized;
+              reject(cleanup('Authorization failed.', error)); // ** Second point of failure.
+            });
+        });
 
-				s.on('error', error => {
-					_._errors.push(error);
-					if (_.enableConsoleLogging) console.error(_.toString(), error);
-				});
+        s.once('end', () => {
+          if (_.enableConsoleLogging)
+            console.warn(this.toString(), 'Disconnected.');
+          _._state = State.Disconnected;
+          cleanup();
+        });
+      });
+    return p;
+  }
 
-				_._send(_.password, PacketType.AUTH).then(() => {
-					_._state = State.Authorized;
-					if (_.enableConsoleLogging) console.log(_.toString(), "Authorized.");
-					resolve(_);
-				}).catch(error => {
-					_._state = State.Unauthorized;
-					reject(cleanup("Authorization failed.", error)); // ** Second point of failure.
-				});
-			});
+  async session<T>(
+    context: (rcon: Rcon, sessionId: number) => Promise<T>
+  ): Promise<T> {
+    const sessionId = ++this._sessionCount;
+    let rcon: Rcon | undefined;
+    try {
+      rcon = await this.connect();
+      return await context(rcon, sessionId);
+    } finally {
+      this._sessionCount--;
+      if (!this._sessionCount && rcon) rcon.disconnect();
+    }
+  }
 
-			s.once('end', () => {
-				if (_.enableConsoleLogging) console.warn(this.toString(), "Disconnected.");
-				_._state = State.Disconnected;
-				cleanup();
-			});
-		});
-		return p;
-	}
+  toString(): string {
+    return `RCON: ${this.host}:${this.port}`;
+  }
 
-	async session<T>(context:(rcon:Rcon,sessionId:number)=>Promise<T>):Promise<T>
-	{
-		const sessionId = ++this._sessionCount;
-		let rcon:Rcon|undefined;
-		try {
-			rcon = await this.connect();
-			return await context(rcon, sessionId);
-		}
-		finally {
-			this._sessionCount--;
-			if(!this._sessionCount && rcon)
-				rcon.disconnect();
-		}
-	}
+  disconnect(): void {
+    const s = this._socket;
+    this._callbacks.clear();
+    if (s) s.end();
+    this._socket = undefined;
+    this._connector = undefined;
+  }
 
-	toString(): string {
-		return `RCON: ${this.host}:${this.port}`;
-	}
+  private _handleResponse(data: Buffer): void {
+    const len = data.readInt32LE(0);
+    if (!len) throw new RconError('Received empty response package');
 
-	disconnect(): void {
-		const s = this._socket;
-		this._callbacks.clear();
-		if (s) s.end();
-		this._socket = undefined;
-		this._connector = undefined;
-	}
+    let id = data.readInt32LE(4);
+    const type = data.readInt32LE(8);
+    const callbacks = this._callbacks;
+    const authId = this._authPacketId;
 
-	private _handleResponse(data: Buffer): void {
-		const len = data.readInt32LE(0);
-		if (!len) throw new RconError('Received empty response package');
+    if (id === -1 && !isNaN(authId) && type === PacketType.RESPONSE_AUTH) {
+      const callback = callbacks.get(authId);
+      if (callback) {
+        id = authId;
+        this._authPacketId = NaN;
+        callback(null, new RconError('Authentication failed.'));
+        callbacks.delete(id);
+      }
+    } else {
+      const callback = callbacks.get(authId);
+      if (callback) {
+        let str = data.toString('utf8', 12, len + 2);
+        if (str.charAt(str.length - 1) === '\n')
+          str = str.substring(0, str.length - 1);
 
-		let id = data.readInt32LE(4);
-		const type = data.readInt32LE(8);
-		const callbacks = this._callbacks;
-		const authId = this._authPacketId;
+        callback(str);
+        callbacks.delete(id);
+      }
+    }
+  }
 
-		if (id === -1 && !isNaN(authId) && type === PacketType.RESPONSE_AUTH) {
-			const callback = callbacks.get(authId);
-			if (callback) {
-				id = authId;
-				this._authPacketId = NaN;
-				callback(null, new RconError('Authentication failed.'));
-				callbacks.delete(id);
-			}
-		}
-		else {
-			const callback = callbacks.get(authId);
-			if (callback) {
-				let str = data.toString('utf8', 12, len + 2);
-				if (str.charAt(str.length - 1) === '\n')
-					str = str.substring(0, str.length - 1);
+  async send(data: string): Promise<string> {
+    if (!this._connector || this._state <= 0)
+      throw new RconError('Instance is not connected.');
 
-				callback(str);
-				callbacks.delete(id);
-			}
-		}
-	}
+    await this._connector;
+    return await this._send(data, PacketType.COMMAND);
+  }
 
-	async send(data: string): Promise<string> {
-		if (!this._connector || this._state <= 0)
-			throw new RconError('Instance is not connected.');
+  private async _send(data: string, cmd: number): Promise<string> {
+    const s = this._socket;
+    if (!s || this._state <= 0)
+      throw new RconError('Instance was disconnected.');
 
-		await this._connector;
-		return await this._send(data, PacketType.COMMAND);
-	}
+    const length = Buffer.byteLength(data);
+    const id = ++this._lastRequestId;
+    if (cmd === PacketType.AUTH) this._authPacketId = id;
 
-	private async _send(data: string, cmd: number): Promise<string> {
-		const s = this._socket;
-		if (!s || this._state <= 0)
-			throw new RconError('Instance was disconnected.');
+    const buf = Buffer.allocUnsafe(length + 14);
+    buf.writeInt32LE(length + 10, 0);
+    buf.writeInt32LE(id, 4); // Not sure how this is used or needed.
+    buf.writeInt32LE(cmd, 8);
+    buf.write(data, 12);
+    buf.fill(0x00, length + 12);
 
-		const length = Buffer.byteLength(data);
-		const id = ++this._lastRequestId;
-		if (cmd === PacketType.AUTH) this._authPacketId = id;
+    await s.write(buf, 'binary');
 
-		const buf = Buffer.allocUnsafe(length + 14);
-		buf.writeInt32LE(length + 10, 0);
-		buf.writeInt32LE(id, 4); // Not sure how this is used or needed.
-		buf.writeInt32LE(cmd, 8);
-		buf.write(data, 12);
-		buf.fill(0x00, length + 12);
+    return await new Promise<string>((resolve, reject) => {
+      const cleanup = () => {
+        clearTimeout(timeout);
+        s.removeListener('end', onEnded);
+        this._callbacks.delete(id);
+        if (cmd === PacketType.AUTH) this._authPacketId = NaN;
+      };
 
-		await s.write(buf, 'binary');
+      const timeout = setTimeout(() => {
+        cleanup();
+        reject(new RconError('Request timed out'));
+      }, this.timeout);
 
-		return await new Promise<string>((resolve, reject) => {
+      const onEnded = () => {
+        cleanup();
+        reject(new RconError('Disconnected before response.'));
+      };
 
-			const cleanup = () => {
-				clearTimeout(timeout);
-				s.removeListener('end', onEnded);
-				this._callbacks.delete(id);
-				if (cmd === PacketType.AUTH) this._authPacketId = NaN;
-			};
+      s.once('end', onEnded);
 
-			const timeout = setTimeout(() => {
-				cleanup();
-				reject(new RconError('Request timed out'));
-			}, this.timeout);
+      this._callbacks.set(id, (data, err) => {
+        cleanup();
 
-			const onEnded = () => {
-				cleanup();
-				reject(new RconError('Disconnected before response.'));
-			};
-
-			s.once('end', onEnded);
-
-			this._callbacks.set(id, (data, err) => {
-				cleanup();
-
-				if (err) reject(err);
-				if (data == null) reject(new RconError("No data returned."));
-				else resolve(data);
-			});
-		});
-	}
+        if (err) reject(err);
+        if (data == null) reject(new RconError('No data returned.'));
+        else resolve(data);
+      });
+    });
+  }
 }
 
 export default Rcon;
